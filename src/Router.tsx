068b21455a0f4c13724e8ac88a2dded531108eab
Lucas Prazeres <lucascprazeres@outlook.com>
2022-08-17 08:34:15 -0300

    feat(routing): setup react-router-dom

---
+++ src/Router.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from 'react-router-dom'
+import { Home, History } from './pages'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/history" element={<History />} />
+    </Routes>
+  )
+}
