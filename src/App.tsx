2812c2ac277828eb9a7abaf7fa1725ed36b7e2b7
Lucas Prazeres <lucascprazeres@outlook.com>
2022-08-17 08:18:39 -0300

    chore: setup project structure

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+
+import { defaultTheme } from './styles/themes'
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello world</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+export default App
