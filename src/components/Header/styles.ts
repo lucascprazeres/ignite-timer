7f5447e23f67c522ce362a5dd06d7c83db447370
Lucas Prazeres <lucascprazeres@outlook.com>
2022-08-18 20:34:29 -0300

    feat(header): add header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,35 @@
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const Navigation = styled.nav`
+  display: flex;
+  gap: 0.5rem;
+`
+
+export const Link = styled(NavLink)`
+  width: 3rem;
+  height: 3rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: ${(props) => props.theme['gray-100']};
+
+  border-top: 3px solid transparent;
+  border-bottom: 3px solid transparent;
+
+  &:hover {
+    border-bottom: 3px solid ${(props) => props.theme['green-500']};
+  }
+
+  &.active {
+    color: ${(props) => props.theme['green-500']};
+  }
+`
