2b95e5dd167c5ba3971a58715f309f43f036d59e
Lucas Prazeres <lucascprazeres@outlook.com>
2022-08-30 22:26:00 -0300

    refactor(home): substitui ternarios por jsx control statements

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,1 @@
-/* eslint-disable no-unused-vars */
-
 /// <reference types="vite/client" />
-
-declare function If(condition: any): any
-declare const Choose: any
-declare function When(condition: any): any
-declare const Otherwise: any
